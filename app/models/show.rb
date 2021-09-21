fff2ce80aa44a7807c49642ea45d0c3a44db8d13
Daniel Suarez <suarez.daniel101@gmail.com>
2021-09-20 22:46:39 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,14 @@
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.find_by(rating: self.highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+end
